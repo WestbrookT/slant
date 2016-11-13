662a2dee62226634cbee70078dc5bf3d0dc3c909
rbalakit <rico@balak.it>
2016-11-12 19:55:26 -0600

    merge and update the controller

--- redtoblue.py
+++ redtoblue.py
@@ -1,12 +1,17 @@
-index = 0.5
-r = int(255 * (1-index))
-g = int(255 * index)
-if (g > 128):
-    g = 255-g
+def indextohex(index):
+    index = 1
+    r = int(255 * (1-index))
+    g = int(255 * index)
+    if (g > 128):
+        g = 255-g
+    b = int(255 * index)
+    rhex = format(r,'x')
+    ghex = format(g,'x')
+    bhex = format(b,'x')
+    return(str(rhex)+str(ghex)+str(bhex))
 
-b = int(255 * index)
-
-rhex = format(r,'x')
-ghex = format(g,'x')
-bhex = format(b,'x')
-print(str(rhex)+str(ghex)+str(bhex))
+print(indextohex(0))
+print(indextohex(.2))
+print(indextohex(.5))
+print(indextohex(.8))
+print(indextohex(1))
