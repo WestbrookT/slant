7b7a8d04de1641f41d5ad98e6750e4f999fee5a6
sphassan <sphassan@sbcglobal.net>
2016-11-12 20:27:57 -0600

    neutral corpus

--- cleanup.py
+++ cleanup.py
@@ -1,8 +1,9 @@
 import json
+from os import listdir
 import sys
 
 articles = json.load(open(sys.argv[1]))
-cleanarticles = {'left' : {}, 'right' : {}}
+cleanarticles = {'left' : {}, 'right' : {}, 'neutral' : {}}
 
 for url in articles['left']:
 	print(url)
@@ -13,5 +14,10 @@ for url in articles['right']:
 	print(url)
 	if not articles['right'][url] == "":
 		cleanarticles['right'][url] = articles['right'][url]
+
+for url in cleanarticles['neutral']:
+	print(url)
+	if not cleanarticles['neutral'][url] == "":
+		cleanarticles['neutral'][url] = articles['neutral'][url]
 
 open('cleanarticles.json', 'w').write(json.dumps(cleanarticles))
