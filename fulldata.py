0d50977aac6bb399a9e10aae1ba84ae087320693
sphassan <sphassan@sbcglobal.net>
2016-11-12 22:21:08 -0600

    fulldata

--- fulldata.py
+++ fulldata.py
@@ -1,4 +1,22 @@
 import json
+import newspaper
+from newspaper import Article
 
 articles = json.load(open('cleanarticles.json'))
-fulldata = [[][][]]
+fulldata = {}
+
+for lean in articles:
+	for url in articles[lean]:
+		print(str(url))
+		article = Article(url)
+		article.download()
+		try:
+			article.parse()
+		except newspaper.article.ArticleException:
+			continue
+		image = article.top_image
+		fulldata[url] = {}
+		fulldata[url]["image"] = image
+		fulldata[url]["text"] = article.text
+
+open('fulldata.json', 'w').write(json.dumps(fulldata))
